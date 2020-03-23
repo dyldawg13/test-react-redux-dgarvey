cdf836e95a86aaca9210291888cd9b4138a5cea4
Dylan Garvey <dgarvey@cadent.tv>
2020-03-23 16:00:07 -0400

    working table w/dummy data

--- src/components/ListTable.jsx
+++ src/components/ListTable.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+
+const columns = [
+  {
+    dataField: "id",
+    text: "Product ID"
+  },
+  {
+    dataField: "name",
+    text: "Product Name"
+  },
+  {
+    dataField: "price",
+    text: "Product Price"
+  }
+];
+const products = [
+  { id: 1, name: "item name 0", price: 2100 },
+  { id: 2, name: "item name 1", price: 2100 },
+  { id: 3, name: "item name 2", price: 2100 }
+];
+const selectRow = {
+  mode: "radio",
+  clickToSelect: true
+};
 
 export const ListTable = () => (
   <div className="listTable">
-    ListTable
+    <BootstrapTable
+      keyField="id"
+      data={products}
+      columns={columns}
+      selectRow={selectRow}
+    />
   </div>
 );
 
-export default ListTable
+export default ListTable;
